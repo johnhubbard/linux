432b4c2fc099b0ff7efbf947fad71e47c7861107
Alistair Popple <apopple@nvidia.com>
2025-06-26 12:52:10 +1000

    nova-core: gsp: Create wpr metadata

--- drivers/gpu/nova-core/gsp.rs
+++ drivers/gpu/nova-core/gsp.rs
@@ -2,15 +2,81 @@
 
 use kernel::bindings;
 use kernel::device;
+use kernel::dma::CoherentAllocation;
+use kernel::dma_write;
 use kernel::pci;
 use kernel::prelude::*;
+use kernel::transmute::{AsBytes, FromBytes, FromBytesSized};
 
 use crate::dma::DmaObject;
+use crate::fb::FbLayout;
+use crate::firmware::Firmware;
 use crate::nvfw::r570_144 as fw;
 
 pub(crate) const GSP_PAGE_SHIFT: usize = 12;
 pub(crate) const GSP_PAGE_SIZE: usize = 1 << GSP_PAGE_SHIFT;
 pub(crate) const GSP_HEAP_SHIFT: u64 = 1 << 20;
+
+unsafe impl FromBytesSized for fw::GspFwWprMeta {}
+unsafe impl AsBytes for fw::GspFwWprMeta {}
+unsafe impl FromBytesSized for fw::GspSystemInfo {}
+unsafe impl AsBytes for fw::GspSystemInfo {}
+
+pub(crate) fn build_wpr_meta(
+    dev: &device::Device<device::Bound>,
+    fw: &Firmware,
+    fb_layout: &FbLayout,
+) -> Result<CoherentAllocation<fw::GspFwWprMeta>> {
+    let wpr_meta =
+        CoherentAllocation::<fw::GspFwWprMeta>::alloc_coherent(dev, 1, GFP_KERNEL | __GFP_ZERO)?;
+    dma_write!(
+        wpr_meta[0] = fw::GspFwWprMeta {
+            magic: fw::GSP_FW_WPR_META_MAGIC as u64,
+            revision: fw::GSP_FW_WPR_META_REVISION as u64,
+            sysmemAddrOfRadix3Elf: fw.gsp.lvl0_dma_handle() as u64,
+            sizeOfRadix3Elf: fw.gsp.size() as u64,
+            sysmemAddrOfBootloader: fw.bootloader.ucode.dma_handle(),
+            sizeOfBootloader: fw.bootloader.ucode.size() as u64,
+            bootloaderCodeOffset: fw.bootloader.code_offset as u64,
+            bootloaderDataOffset: fw.bootloader.data_offset as u64,
+            bootloaderManifestOffset: fw.bootloader.manifest_offset as u64,
+            __bindgen_anon_1: fw::GspFwWprMeta__bindgen_ty_1 {
+                __bindgen_anon_1: fw::GspFwWprMeta__bindgen_ty_1__bindgen_ty_1 {
+                    sysmemAddrOfSignature: fw.gsp_sigs.dma_handle() as u64,
+                    sizeOfSignature: fw.gsp_sigs.size() as u64,
+                }
+            },
+            gspFwRsvdStart: fb_layout.heap.start,
+            nonWprHeapOffset: fb_layout.heap.start,
+            nonWprHeapSize: fb_layout.heap.end - fb_layout.heap.start,
+            gspFwWprStart: fb_layout.wpr2.start,
+            gspFwHeapOffset: fb_layout.wpr2_heap.start,
+            gspFwHeapSize: fb_layout.wpr2_heap.end - fb_layout.wpr2_heap.start,
+            gspFwOffset: fb_layout.elf.start,
+            bootBinOffset: fb_layout.boot.start,
+            frtsOffset: fb_layout.frts.start,
+            frtsSize: fb_layout.frts.end - fb_layout.frts.start,
+            gspFwWprEnd: fb_layout.vga_workspace.start & !(0x20000 - 1),
+            gspFwHeapVfPartitionCount: fb_layout.vf_partition_count,
+            fbSize: fb_layout.fb.end - fb_layout.fb.start,
+            vgaWorkspaceOffset: fb_layout.vga_workspace.start,
+            vgaWorkspaceSize: fb_layout.vga_workspace.end - fb_layout.vga_workspace.start,
+            bootCount: 0,
+            __bindgen_anon_2: fw::GspFwWprMeta__bindgen_ty_2 {
+                __bindgen_anon_1: fw::GspFwWprMeta__bindgen_ty_2__bindgen_ty_1 {
+                    partitionRpcAddr: 0,
+                    partitionRpcRequestOffset: 0,
+                    partitionRpcReplyOffset: 0,
+                    ..Default::default()
+                },
+            },
+            verified: 0,
+            ..Default::default()
+        }
+    )?;
+
+    Ok(wpr_meta)
+}
 
 #[allow(unused)]
 pub(crate) struct GspMemObjects {
@@ -57,6 +123,35 @@ fn create_dma_object(
 ) -> Result<DmaObject> {
     let mut obj = DmaObject::new(dev, size)?;
     create_pte_array(&mut obj);
+
+    let arg_offset = libos_arg_nr * size_of::<fw::LibosMemoryRegionInitArgument>();
+    let libos_start_ptr = unsafe { libos.start_ptr_mut().add(arg_offset) };
+
+    let libos_mem_init_args = fw::LibosMemoryRegionInitArgument {
+        id8: id8(name),
+        pa: obj.dma_handle(),
+        size: obj.size() as u64,
+        kind: fw::LibosMemoryRegionKind_LIBOS_MEMORY_REGION_CONTIGUOUS as u8,
+        loc: fw::LibosMemoryRegionLoc_LIBOS_MEMORY_REGION_LOC_SYSMEM as u8,
+    };
+    unsafe {
+        core::ptr::copy_nonoverlapping(
+            &libos_mem_init_args as *const fw::LibosMemoryRegionInitArgument,
+            libos_start_ptr as *mut fw::LibosMemoryRegionInitArgument,
+            1,
+        );
+    };
+
+    Ok(obj)
+}
+
+fn create_coherent_dma_object<A: AsBytes + FromBytes>(
+    dev: &device::Device<device::Bound>,
+    name: &'static str,
+    libos: &mut DmaObject,
+    libos_arg_nr: usize,
+) -> Result<CoherentAllocation<A>> {
+    let obj = CoherentAllocation::<A>::alloc_coherent(dev, 1, GFP_KERNEL | __GFP_ZERO)?;
 
     let arg_offset = libos_arg_nr * size_of::<fw::LibosMemoryRegionInitArgument>();
     let libos_start_ptr = unsafe { libos.start_ptr_mut().add(arg_offset) };
